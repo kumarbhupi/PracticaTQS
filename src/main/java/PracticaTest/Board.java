d20f31b8c44b0e047362af0f0d75dffb8ce83722
JONATHAN-PPC\Jonathan <jmxlina@gmail.com>
2020-11-15 10:16:16 +0100

    Board improves

--- src/main/java/PracticaTest/Board.java
+++ src/main/java/PracticaTest/Board.java
@@ -1,51 +1,124 @@
 package PracticaTest;
 
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Board {
 
 	private Square[][] boardMatrix;
 	int mineNum;
+	int remaining = 14 * 18 - 40;
 	
 	Board() {
-		//boardMatrix = new Square[14][18];
+		boardMatrix = new Square[14][18];
+		this.clearBoard();
 		
 	}
-	////////////FOR TESTING PURPOSES//////////// 
+	////////////FOR TESTING PURPOSES////////////  
 	Board(boolean[][] inputMat) {
-	
+		boardMatrix = new Square[inputMat.length][inputMat[0].length];
+		this.generateFromBoolMatrix(inputMat);
 	}
 	////////////////////////////////////////////
 	
 	private void clearBoard() {
-
+		
+		for(int i=0; i < this.getLengthY();i++) {
+			for(int j=0; j < this.getLengthX();j++) {
+				boardMatrix[i][j] = new Square();
+				
+			}
+		}
 		
 	}
 	
-	private void generateMineMap(int nMines) {
+	public void generateMineMap(int nMines) {
 		
-
+		int upperboundX = this.getLengthX()-1;
+		int upperboundY = this.getLengthY()-1;
+		int xAxis; 
+	    int yAxis;
+	    
+		Random rand = new Random();
+		
+		int placedMines = 0;
+		
+		while(placedMines != nMines) {
+			xAxis = rand.nextInt(upperboundX); 
+		    yAxis = rand.nextInt(upperboundY);
+			
+			if (!isMine(xAxis, yAxis)) {
+				boardMatrix[yAxis][xAxis].setMine();
+				placedMines++;
+			}
+		}
 		
 	}
+	////////////FOR TESTING PURPOSES//////////// 
+	public void generateFromBoolMatrix(boolean[][] inputMat) {
+		
+		for(int i=0; i < this.getLengthY();i++) {
+			for(int j=0; j < this.getLengthX();j++) {
+				boardMatrix[i][j] = new Square();
+				
+				if (inputMat[i][j])
+					boardMatrix[i][j].setMine();
+				
+			}
+		}
+		
+	}
+	////////////////////////////////////////////
 	
 	//si peta -1
 	//si no peta, el numero de casillas que descubre
 	public int uncoverPosition(int x, int y) {
 		
-		return 0;// tal vez el número de casillas para luego usarlo en la puntuación?
+		//ArrayList<int[]> checkedPos = new ArrayList<>();
+		
+		if(boardMatrix[y][x].isMine()) {
+			boardMatrix[y][x].setState(-3);
+			return -1;
+		} else {
+			boardMatrix[y][x].setState(-1);
+			return 1;
+			//checkedPos.add(new int[] {x,y});
+			//uncoverNeighbours(x, y);
+		}
+		
+		// tal vez el número de casillas para luego usarlo en la puntuación?
 	}
 	
 	private void uncoverNeighbours(int x, int y) {
 
 	}
 	
+	public boolean isAllUncovered() {
+		
+		return remaining == 0;
+	}
+	
 	public boolean setFlag(int x, int y) {
 		
-		 return false;//cuando hay bandera y vuelven a llamar a set bandera
+		if(this.getState(x, y) == -2) {
+			setState(x, y, 0);
+			return false;
+		} else {
+			setState(x, y, -2);
+			return true;
+		}
+		
+		
 	}
 	
 	public int getState(int x, int y) {
 		
-		return 0; 
+		return boardMatrix[y][x].getState();
+	}
+	
+	public void setState(int x, int y, int state) {
+		
+		boardMatrix[y][x].setState(state);
 	}
 	
 	public boolean isMine(int x, int y) {
@@ -60,10 +133,9 @@ public class Board {
 	
 	public void proxyClearBoard() { clearBoard();}
 	public void proxyGenerateMineMap(int nMines) { generateMineMap(nMines);}	
-	//public boolean proxyChangeMineSate() { return changeMineSate();}
-	public int proxyUncoverPosition(int x, int y) { return uncoverPosition(x, y);}
-	public void proxyUncoverNeighbours(int x, int y) { uncoverNeighbours(x, y);}	
+	public int proxyUncoverPosition(int x, int y) { return uncoverPosition(x, y);}	
 	public void proxyToggleBanderaByPosition(int x, int y) { setFlag(x, y);}
+	
 	
 	
 }
